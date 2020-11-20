ac951d0ed0d3f8deb0dfdb492c5b8ce699e9b378
Niklas Wernfeldt <niklas.wernfeldt.94@gmail.com>
2020-11-20 11:51:00 +0100

    test4

--- bin/posts-mock-data.js
+++ bin/posts-mock-data.js
@@ -1,34 +1,34 @@
 const posts = [
     {
-        image: "https://cdn.pixabay.com/photo/2016/02/19/11/19/office-1209640__340.jpg"
-        title: "Working hard"
+        image: "https://cdn.pixabay.com/photo/2016/02/19/11/19/office-1209640__340.jpg",
+        title: "Working hard",
         user: [{ type: mongoose.SchemaTypes.ObjectId, ref: "User" }],
-        description: "Another day at the office with my awesome colleagues."
-        date: "2020-10-25"
+        description: "Another day at the office with my awesome colleagues.",
+        date: "2020-10-25",
     },
     {
-        image: "https://cdn.pixabay.com/photo/2016/01/19/17/41/friends-1149841_1280.jpg"
-        title: "Cat & Dog"
+        image: "https://cdn.pixabay.com/photo/2016/01/19/17/41/friends-1149841_1280.jpg",
+        title: "Cat & Dog",
         user: [{ type: mongoose.SchemaTypes.ObjectId, ref: "User" }],
-        description: "Human's best friends"
-        date: "2020-10-26"
+        description: "Human's best friends",
+        date: "2020-10-26",
     },
     {
-        image: "https://cdn.pixabay.com/photo/2016/03/09/09/42/buildings-1245953_1280.jpg"
-        title: "Night life"
+        image: "https://cdn.pixabay.com/photo/2016/03/09/09/42/buildings-1245953_1280.jpg",
+        title: "Night life",
         user: [{ type: mongoose.SchemaTypes.ObjectId, ref: "User" }],
-        description: ""
-        date: "2020-10-27"
+        description: "",
+        date: "2020-10-27",
     },
     {
-        image: "https://cdn.pixabay.com/photo/2016/03/09/11/49/man-1246233_1280.jpg"
-        title: "Nature"
+        image: "https://cdn.pixabay.com/photo/2016/03/09/11/49/man-1246233_1280.jpg",
+        title: "Nature",
         user: [{ type: mongoose.SchemaTypes.ObjectId, ref: "User" }],
-        description: ""
-        date: "2020-10-28"
+        description: "",
+        date: "2020-10-28",
     },
     {
-        image: "https://cdn.pixabay.com/photo/2015/07/30/17/24/audience-868074_1280.jpg"
+        image: "https://cdn.pixabay.com/photo/2015/07/30/17/24/audience-868074_1280.jpg",
         title: "Concert"
         user: [{ type: mongoose.SchemaTypes.ObjectId, ref: "User" }],
         description: "Amazing night with you guys"
---
+++ bin/seed.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const User = require("./../models/User.model");
+// const Post = require("./../models/Post.model");
+
+const users = require("./users-mock-data");
+// const posts = require("./posts-mock-data");
+
+const DB_NAME = "library";
+
+// SEED SEQUENCE
+
+// 0. ESTABLISH CONNECTION TO MONGO DATABASE
+mongoose
+  .connect(`mongodb://localhost:27017/${DB_NAME}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then((x) => {
+    // 1. DROP THE DATABASE
+    const pr = x.connection.dropDatabase();
+
+    return pr;
+  })
+  .then(() => {
+    // 2.  CREATE THE DOCUMENTS FROM ARRAY OF authors
+    const pr = User.create(users);
+    return pr; // forwards the promise to next `then`
+  })
+  .then((createdUsers) => {
+    console.log(`Created ${createdUsers.length} authors`);
+
+    // 3. WHEN .create() OPERATION IS DONE
+    // UPDATE THE OBJECTS IN THE ARRAY OF books
+
+    const updatedUsers = users.map((userObj, i) => {
+      // Update the userObj and set the corresponding author id
+      // to create the reference
+      const user = createdUsers[i];
+      userObj.posts = [user._id];
+
+      return userObj; // return the updated bookObj
+    });
+
+    const pr = Post.create(updatedUsers);
+    return pr; // forwards the promise to next `then`
+  })
+  .then((createdBooks) => {
+    // 4. WHEN .create() OPERATION IS DONE, CLOSE DB CONNECTION
+    console.log(`Inserted ${createdBooks.length} books`);
+
+    mongoose.connection.close();
+  })
+  .catch((err) => console.log(err));
