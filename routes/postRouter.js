2a79d73f988f72b982b3a7cef4cce86301762310
Ferran Martinez <ferran.ms8@gmail.com>
2020-11-23 17:38:06 +0100

    not working

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const postRouter = express.Router();
+const User = require("../models/User.model");
+const Post = require("../models/Post.model");
+const isLoggedIn = require("../utils/isLoggedIn");
 
-postRouter.get("/", (req, res, next) => {
+postRouter.get("/username", (req, res, next) => {
+  const {_id} = req.session.currentUser;
+  User.findById(_id)
+    .populate('posts')
+    .then((user)  => {
+      console.log('user posts', user.posts);
+    })
+  
+
   res.render("Post");
 });
 
-postRouter.get("/:id");
 
 module.exports = postRouter;
