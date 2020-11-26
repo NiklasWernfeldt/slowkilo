bedc01f431e9a336a85b8e87833674514b317793
Niklas Wernfeldt <niklas.wernfeldt.94@gmail.com>
2020-11-26 10:58:32 +0100

    continue in the morning

--- views/Feed.jsx
+++ views/Feed.jsx
@@ -6,14 +6,16 @@ function Feed(props) {
   return (
     <Layout>
       <div>
-        <h1>Heloo</h1>
+        <h1>Heloo from feed</h1>
         {props.post.map((post, i) => {
           return (
             <div key={i}>
               <a href={`/posts/user/${props.post[i].user._id}`}>
                 <p>{props.post[i].user.username}</p>
               </a>
-             <a href={`/posts/details/${post._id}`}><img src={post.image} alt="feed-image" /></a>
+              <a href={`/posts/details/${post._id}`}>
+                <img src={post.image} alt="feed-image" />
+              </a>
               <h3>{post.title}</h3>
               <p>{post.description}</p>
             </div>
